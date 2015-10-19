9f752721cfcd326381b44f4a4b3796345681986d
Adam Weitzman <adamcweitzman@gmail.com>
2015-10-19 15:16:06 -0500

    home view created

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-<<<<<<< HEAD
-  res.render('index', { title: 'Express' });
-=======
   res.render('index', { title: 'Bartinder' });
->>>>>>> 32a6606a6ebfef799075743367575d00c393ae74
+
 });
+
+// Get another picture when an arrow is hit
+router.get('/home', function(req, res) {
+	
+}
 
 module.exports = router;
