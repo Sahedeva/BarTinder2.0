d2fba973267cbc4ca84c134bfe90dad8bd83661d
Robert Weilbaecher <robertweilbaecher@hotmail.com>
2015-10-20 17:56:05 -0500

    got token to user yay

--- public/javascripts/bartinder.js
+++ public/javascripts/bartinder.js
@@ -1,5 +1,33 @@
 
-document.on('click', function() {
-  console.log('bartinder');
+// $(document).on('click', function() {
+//   console.log('bartinder');
+
+// });
+
+
+$('.login_form').on('submit', function(){
+			var name = $('#name').val();
+			var password = $('#password').val();
+ 			$.ajax({
+            url: 'http://localhost:3000/authenticate',
+            dataType: "json",
+            method: "POST",
+            data: {
+            	name: name,
+            	password: password
+            	
+          		},
+            success: function(data, textStatus, jqXHR){
+               console.log("hi");
+               console.log(data);
+               // var token = data['token'];
+               // console.log(token);
+            },
+            error: function(XMLHttpRequest, textStatus, errorThrown){
+            		alert("Status: " + textStatus); alert("Error: " + errorThrown);
+            }
+         });
+
+    return false
 
 });
