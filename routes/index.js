3e7ec6dcc8a7a2b5a61d6dc5ac5250a4b8299db9
Robert Weilbaecher <robertweilbaecher@hotmail.com>
2015-10-21 14:42:32 -0500

    back to random

--- routes/index.js
+++ routes/index.js
@@ -163,8 +163,8 @@ router.post('/authenticate', function(req, res) {
 router.get('/home', function(req, res) {
 	Venue.find({}, function(err, venues) {
 // Get back a random variable from the database so you can display on page
-	// venue = venues[Math.floor(Math.random()*venues.length)]
-  venue = venues[7];
+	venue = venues[Math.floor(Math.random()*venues.length)];
+  
 	console.log(venue);
 
 	res.render('home');
